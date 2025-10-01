fa9b444825b5465710a58aaa7f58273ad282c3e4
sultanadika <sultanmumtazsami@gmail.com>
2025-10-01 13:56:56 +0700

    Tutorial AJAX

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,16 @@
 from django.forms import ModelForm
 from .models import News   # relative import
+from django.utils.html import strip_tags
 
 class NewsForm(ModelForm):
     class Meta:
         model = News
         fields = ["title", "content", "category", "thumbnail", "is_featured"]
-    
+        
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        return strip_tags(title)
+
+    def clean_content(self):
+        content = self.cleaned_data["content"]
+        return strip_tags(content)
